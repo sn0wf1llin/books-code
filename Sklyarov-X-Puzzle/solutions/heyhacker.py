cad6baf8248fd7df40f726b20aa319197af35f2b
sn0wf1llin <sn0wf1llin@gmail.com>
2019-07-22 03:43:53 +0300

    ;

--- Sklyarov-X-Puzzle/solutions/heyhacker.py
+++ Sklyarov-X-Puzzle/solutions/heyhacker.py
@@ -17,14 +17,21 @@ def xor_hexes(h1, h2):
 
 
 def invert_and_hexes(h1, h2):
-	result = None
-	print("not work")
+	result = '-'
 	return result
 
 
-def invert_or_hexes(h1, h2):
-	result = None
-	print("not work")
+def guess_or(h1):
+	print("---\n", chr(int(h1, 16)))
+	vars = "\t : "
+	variants = ['00', '01', '10', '11']
+	for pos_var in variants:
+		_ = bin(int(h1, 16))
+		_ = _[2:-2]
+		_ = hex(int(_ + pos_var , 2))
+		vars += chr(int(_, 16)) + " "
+	print(vars)
+	result = '-'
 	return result
 
 
@@ -39,11 +46,11 @@ def decrypt(p):
 
 		elif ind % 3 == 2:
 			r = invert_and_hexes(shex, '0xc8')
-			res += str(r) + " "
+			res += r + " "
 
 		elif ind % 3 == 1:
-			r = invert_or_hexes(shex, '0x3')
-			res += str(r) + " "
+			r = guess_or(shex)
+			res += r + " "
 
 	return res
 
@@ -67,26 +74,38 @@ def crypt(p):
 
 	return res
 
+
+def hex2str_unknown(hexs):
+	res = ""
+	for i in hexs.split(" "):
+		if len(i) > 0:
+			if i != '-':
+				res += chr(int(i, 16))
+			else:
+				res += "-"
+	return res
+
+
 if __name__ == "__main__":
 	p = "Hey Hacker!"
 	r = "zgH↕K@Qk@@#"
 
 	todo = "fkHA#HA#@Gc@[c j/@G{H^g"
 
-	phex = str2hex(p)
-	rhex = str2hex(r)
-	print(phex)
-	print(rhex)
-	xored = xor(phex, rhex, din_format='hex')
-	print("--- xor result ---")
-	print(xored)
-
-	crypted = crypt(p)
-	print("--- crypt result ---")
-	print(crypted)
-	print("--- crypt --> str result ---")
-	crypt_str = hex2str(crypted)
-	print(crypt_str)
+	# phex = str2hex(p)
+	# rhex = str2hex(r)
+	# print(phex)
+	# print(rhex)
+	# xored = xor(phex, rhex, din_format='hex')
+	# print("--- xor result ---")
+	# print(xored)
+	#
+	# crypted = crypt(p)
+	# print("--- crypt result ---")
+	# print(crypted)
+	# print("--- crypt --> str result ---")
+	# crypt_str = hex2str(crypted)
+	# print(crypt_str)
 	# 1. xor 0x32 - 0x32 - 0x32
 	# 2. xor 0x2 - 0x3 - 0x0
 	# 3. xor 0x31 - 0x21 - 0x25
@@ -94,9 +113,10 @@ if __name__ == "__main__":
 	# ---------------
 	# decrypt todo
 	# ---------------
+	print(todo)
 	decrypted = decrypt(todo)
-	print("--- crypt result ---")
-	print(crypted)
-	print("--- crypt --> str result ---")
-	crypt_str = hex2str(crypted)
-	print(crypt_str)
+	print("--- decrypt result ---")
+	print(decrypted)
+	print("--- decrypt --> str result ---")
+	decrypt_str = hex2str_unknown(decrypted)
+	print(decrypt_str)
